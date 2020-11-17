faae2d193b3d6e33783f4e59ec62acdc81895359
Ihor Lytvynchuk <ilytvynchuk@artelogic.net>
2020-11-17 12:11:36 +0200

    ALDE-2841 rename users_info table to user_info

--- database/migrations/2020_11_13_102630_create_user_info_table.php
+++ database/migrations/2020_11_13_102630_create_user_info_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersInfoTable extends Migration
+class CreateUserInfoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersInfoTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_info', function (Blueprint $table) {
+        Schema::create('user_info', function (Blueprint $table) {
             $table->id();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
@@ -33,6 +33,6 @@ class CreateUsersInfoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_info');
+        Schema::dropIfExists('user_info');
     }
 }
