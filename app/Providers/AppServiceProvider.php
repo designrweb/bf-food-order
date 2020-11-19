e22b455f7ce1d45355348468ff6ded0c370c6595
Ihor Lytvynchuk <ilytvynchuk@artelogic.net>
2020-11-19 11:04:35 +0200

    ALDE-2842 added upload image component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        FormRequest::macro('test', function () {
-            return get_class($this);
-        });
+        //
     }
 
     /**
