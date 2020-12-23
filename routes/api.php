5f7f04004159463c20016dcee9b85318f8d1bcbb
Mykhailo Zhvirko <mzvirko@artelogic.net>
2020-12-23 10:37:30 +0200

    ALDE-2940-api-for-pos-application create tests for API routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function() {
+    /** POS terminal routes */
+    Route::prefix('pos')->group(function() {
+        Route::get('menuitem', 'MenuitemController@index');
+    });
+});
---
+++ tests/Feature/Api/Routing/PosApplication/PosApplicationAuthRoutesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Api\Routing\PosApplication;
+
+use Tests\TestCase;
+
+class PosApplicationAuthRoutesTest extends TestCase
+{
+    /** @test */
+    public function itResponsesOnUserLogin()
+    {
+        $response = $this->post('/api/v1/pos/user/login');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function itResponsesOnUserLogout()
+    {
+        $response = $this->post('/api/v1/pos/user/logout');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function itResponsesOnUserData()
+    {
+        $response = $this->get('/api/v1/pos/user/data');
+
+        $response->assertStatus(200);
+    }
+}
