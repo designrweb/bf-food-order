00a9539d4dc863841382b1b33c1e95f8b23c89af
Ihor Lytvynchuk <ilytvynchuk@artelogic.net>
2020-12-29 13:01:10 +0200

    ALDE-2992 deleted temp excel files and updated gitignore file

--- app/Http/ViewComposers/CompanySettingCompose.php
+++ app/Http/ViewComposers/CompanySettingCompose.php
@@ -27,7 +27,7 @@ class CompanySettingCompose
      */
     public function compose(View $view)
     {
-        $settings = auth()->check() ? auth()->user()->userCompany->company->settings->keyBy('setting_name')
+        $settings = !empty(auth()->user()->userCompany) ? auth()->user()->userCompany->company->settings->keyBy('setting_name')
             ->transform(function ($setting) {
                 return $setting->value;
             })->toArray() : [];
