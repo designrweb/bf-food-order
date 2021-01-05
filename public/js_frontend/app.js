9819dae1f672d9fc89a146329fe0dfcc8e9952d1
Ihor Lytvynchuk <ilytvynchuk@artelogic.net>
2021-01-05 15:19:11 +0200

    added devlivey planning form and updated CSS

--- public/js_frontend/app.js
+++ public/js_frontend/app.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 21);
+/******/ 	return __webpack_require__(__webpack_require__.s = 22);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -37321,7 +37321,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ 21:
+/***/ 22:
 /*!***********************************!*\
   !*** multi ./resources/js/app.js ***!
   \***********************************/
