721ddb04f1acc19aff09801ee0cdc71748691c10
Mykhailo Zhvirko <mzvirko@artelogic.net>
2021-01-09 14:32:46 +0200

    ALDE-2866-order refactored ConsumerFactoryTest

--- tests/Unit/Models/Consumer/Factory/ConsumerFactoryTest.php
+++ tests/Unit/Models/Consumer/Factory/ConsumerFactoryTest.php
@@ -5,17 +5,19 @@ namespace Tests\Unit\Models\Consumer\Factory;
 use App\Consumer;
 use Tests\TestCase;
 
+/**
+ * @group factory
+ */
 class ConsumerFactoryTest extends TestCase
 {
     /** @test */
     public function consumer_factory_persists_one_entity_to_database()
     {
-        $consumer = factory(Consumer::class)->create([
+        $consumer = create(Consumer::class, [
             'firstname' => 'New Consumer',
         ]);
 
         $this->assertDatabaseCount($consumer->getTable(), 1);
-
         $this->assertDatabaseHas($consumer->getTable(), [
             'firstname' => 'New Consumer',
         ]);
@@ -24,8 +26,9 @@ class ConsumerFactoryTest extends TestCase
     /** @test */
     public function consumer_factory_persists_many_entities_to_database()
     {
-        factory(Consumer::class, 10)->create();
+        $consumers = create(Consumer::class, [], 10);
 
-        $this->assertDatabaseCount('companies', 10);
+        $this->assertDatabaseCount('consumers', 10);
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $consumers);
     }
 }
