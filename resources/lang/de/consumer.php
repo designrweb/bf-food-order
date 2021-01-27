99665f7e65db869edb270696e41b038dee788a86
Mykhailo Zhvirko <mzvirko@artelogic.net>
2021-01-27 13:15:13 +0200

    ALDE-3072-bugs-and-improvements -- fixed bugs

--- resources/lang/de/consumer.php
+++ resources/lang/de/consumer.php
@@ -3,54 +3,50 @@
  * Translations for the Consumer model
  */
 return [
-    'Consumers'                                                                           => 'Kunde/Essensteilnehmer',
-    'Consumer'                                                                            => 'Essensteilnehmer',
-    'Consumer'            => 'Kunde',
-    'Account'                                                                             => 'Kundennummer',
-    'Parent Salutation'                                                                   => 'Anrede Erziehungsberechtigter',
-    'Firstname'                                                                           => 'Vorname',
-    'Lastname'                                                                            => 'Nachname Erziehungsberechtigter',
-    'Parent'                                                                              => 'Erziehungsberechtigter',
-    'Child Lastname'                                                                      => 'Nachname des Kindes',
-    'Child'                                                                               => 'Kind',
-    'Birthday'                                                                            => 'Geburtstag',
-    'Child Imageurl'                                                                      => 'Foto',
-    'Photo'                                                                               => 'Foto',
-    'Balance'                                                                             => 'Guthaben',
-    'Balance limit'                                                                       => 'Guthaben Grenze',
-    'User ID'                                                                             => 'Benutzer',
-    'QR Code'                                                                             => 'QR-Code',
-    'QR-Code not found. Please generate it.'                                              => 'QR-Code nicht gefunden. Bitte generieren Sie es.',
+    'Consumers'                                                                                                                                                                                    => 'Kunde/Essensteilnehmer',
+    'Consumer'                                                                                                                                                                                     => 'Essensteilnehmer',
+    'Account'                                                                                                                                                                                      => 'Kundennummer',
+    'Parent Salutation'                                                                                                                                                                            => 'Anrede Erziehungsberechtigter',
+    'Firstname'                                                                                                                                                                                    => 'Vorname',
+    'Lastname'                                                                                                                                                                                     => 'Nachname Erziehungsberechtigter',
+    'Parent'                                                                                                                                                                                       => 'Erziehungsberechtigter',
+    'Child Lastname'                                                                                                                                                                               => 'Nachname des Kindes',
+    'Child'                                                                                                                                                                                        => 'Kind',
+    'Birthday'                                                                                                                                                                                     => 'Geburtstag',
+    'Child Imageurl'                                                                                                                                                                               => 'Foto',
+    'Photo'                                                                                                                                                                                        => 'Foto',
+    'Balance'                                                                                                                                                                                      => 'Guthaben',
+    'Balance limit'                                                                                                                                                                                => 'Guthaben Grenze',
+    'User ID'                                                                                                                                                                                      => 'Benutzer',
+    'QR Code'                                                                                                                                                                                      => 'QR-Code',
+    'QR-Code not found. Please generate it.'                                                                                                                                                       => 'QR-Code nicht gefunden. Bitte generieren Sie es.',
     "<p>Please transfer to the following account: 
     <br>Receiver: Lehmanns
     <br>Credit institution: Sparkasse KölnBonn
     <br>IBAN: <strong>DE51 3705 0198 1935 3522 27</strong>
     <br>BIC: COLSDE33 
-    <br>Purpose: full name of the child and customer number <strong>{0}</strong></p>" => "<p>Bitte überweisen Sie an folgendes Konto:
+    <br>Purpose: full name of the child and customer number <strong>{0}</strong></p>"                                                                                                          => "<p>Bitte überweisen Sie an folgendes Konto:
                                                                                             <br>Empfänger: Lehmanns
                                                                                             <br>Kreditinstitut: Sparkasse KölnBonn
                                                                                             <br>IBAN: <strong>DE51 3705 0198 1935 3522 27</strong>
                                                                                             <br>BIC: <strong>COLSDE33</strong>
                                                                                             <br>Verwendungszweck: Vollständiger Name des Kindes und Kundennummer (<strong>{0}</strong>)</p>",
-    'Generate'                                                                            => 'Generieren',
-    'Regenerate'                                                                          => 'Regenerieren',
-    'Set your child\'s birthday...'                                                       => 'Stellen Sie den Geburtstag Ihres Kindes ein ...',
-    'Consumer has been successfully created.'                                             => 'Essensteilnehmer wurde erfolgreich erstellt.',
-    'Consumer has been successfully updated.'                                             => 'Essensteilnehmer wurde erfolgreich aktualisiert.',
-    'Update Consumer: {name}'                                                             => 'Aktualisiere Benutzer: {name}',
-    'Please increase your balance'                                                        => 'Bitte erhöhen Sie Ihr Guthaben',
-    'Notify me if balance is lower than'                                                  => 'Benachrichtigen Sie mich, wenn das Guthaben niedriger als ist',
-    'Manual'                                                                              => 'Druckansicht (PDF)',
-    'Old QR code will be not working anymore. Please print a new one after confirmation.' => 'Der alte QR-Code ist ab sofort deaktiviert. Bitte verwenden Sie nach Bestätigung Ihren neuen QR-Code.',
-    'Credit Limit'                                                                        => 'Mindest-Guthaben',
-    'Create Consumer'                                                                     => 'Benutzerdaten Anlegen',
-    'Select consumer'                                                                     => 'Benutzer auswählen',
-    'Consumer not inserted due to validation error.'                                      => 'Essensteilnehmer wegen Validierungsfehler nicht eingefügt.',
-
+    'Generate'                                                                                                                                                                                     => 'Generieren',
+    'Regenerate'                                                                                                                                                                                   => 'Regenerieren',
+    'Set your child\'s birthday...'                                                                                                                                                                => 'Stellen Sie den Geburtstag Ihres Kindes ein ...',
+    'Consumer has been successfully created.'                                                                                                                                                      => 'Essensteilnehmer wurde erfolgreich erstellt.',
+    'Consumer has been successfully updated.'                                                                                                                                                      => 'Essensteilnehmer wurde erfolgreich aktualisiert.',
+    'Update Consumer'                                                                                                                                                                              => 'Aktualisiere Benutzer: :name',
+    'Please increase your balance'                                                                                                                                                                 => 'Bitte erhöhen Sie Ihr Guthaben',
+    'Notify me if balance is lower than'                                                                                                                                                           => 'Benachrichtigen Sie mich, wenn das Guthaben niedriger als ist',
+    'Manual'                                                                                                                                                                                       => 'Druckansicht (PDF)',
+    'Old QR code will be not working anymore. Please print a new one after confirmation.'                                                                                                          => 'Der alte QR-Code ist ab sofort deaktiviert. Bitte verwenden Sie nach Bestätigung Ihren neuen QR-Code.',
+    'Credit Limit'                                                                                                                                                                                 => 'Mindest-Guthaben',
+    'Create Consumer'                                                                                                                                                                              => 'Benutzerdaten Anlegen',
+    'Select consumer'                                                                                                                                                                              => 'Benutzer auswählen',
+    'Consumer not inserted due to validation error.'                                                                                                                                               => 'Essensteilnehmer wegen Validierungsfehler nicht eingefügt.',
     'Please set your `minimum credit` in order to receive a message as soon as your credit falls below this value. You will be informed in good time and you can top up your credit in good time.' => 'Bitte stellen Sie Ihr `Mindest-Guthaben` ein, um eine Nachricht zu erhalten sobald Ihr Guthaben diesen Wert unterschreitet. So werden Sie frühzeitig informiert und können Ihr Guthaben rechtzeitig aufladen.',
-
-    'Consumer ID'       => 'Essensteilnehmer',
-    'Consumer Email'    => 'Essensteilnehmer-E-Mail',
-    'Update Consumer: ' => 'Aktualisiere Benutzer: ',
+    'Consumer ID'                                                                                                                                                                                  => 'Essensteilnehmer',
+    'Consumer Email'                                                                                                                                                                               => 'Essensteilnehmer-E-Mail',
 
 ];
