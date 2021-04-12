94036f8269b0f96da5507f64f2f733d4d8a6794c
Ihor Lytvynchuk <ilytvynchuk@artelogic.net>
2021-04-12 17:11:12 +0300

    ALDE-added trans

--- resources/views/user/food_order/orders.blade.php
+++ resources/views/user/food_order/orders.blade.php
@@ -5,7 +5,7 @@
         <grid-index
                 :main_route="'/user/order'"
                 v-bind:user_consumer_exists="{{ $userConsumerExists ? 'true' : 'false' }}"
-                title="@lang('order.Orders')"
+                title="@lang('order.Food Orders')"
         ></grid-index>
     </div>
 @endsection
